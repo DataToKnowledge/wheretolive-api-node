af7f43b864e38f04486464b09a992dc598f699d1
Fumarola Fabio (UniCredit) <fabio.fumarola@unicredit.eu>
2016-04-09 23:24:16 +0200

    update

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ var swaggerDoc = jsyaml.safeLoad(spec);
 // Initialize the Swagger middleware
 swaggerTools.initializeMiddleware(swaggerDoc, function (middleware) {
 
-  app.use(function(req, res){
+  app.use(function(req, res, next){
     // Set CORS headers
     	res.setHeader('Access-Control-Allow-Origin', '*');
     	res.setHeader('Access-Control-Request-Method', '*');
@@ -33,6 +33,7 @@ swaggerTools.initializeMiddleware(swaggerDoc, function (middleware) {
     		res.end();
     		return;
     	};
+      next();
   });
 
   // Interpret Swagger resources and attach metadata to request - must be first in swagger-tools middleware chain
